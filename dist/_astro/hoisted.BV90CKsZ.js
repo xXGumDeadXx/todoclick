67fd1d39f5358c12b4ac7d5be258a846cbc602e7
xXGumDeadXx <angeljlinarez@gmail.com>
2024-07-27 22:11:28 -0400

    x

--- dist/_astro/hoisted.BV90CKsZ.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.DtcEVq1D.js";document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("prevBtn"),n=document.getElementById("nextBtn"),e=document.querySelector(".service-container");t.addEventListener("click",()=>{e.scrollBy({top:0,left:-340,behavior:"smooth"})}),n.addEventListener("click",()=>{e.scrollBy({top:0,left:340,behavior:"smooth"})})});
